26c14ceb89d595aa5708709751e3fcccd6382225
Nome do usuário <email@dominio.com>
2024-07-26 09:33:17 -0300

    Sw.js atualização

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.spu0fch1nr"
+    "revision": "0.m6601qkirpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
